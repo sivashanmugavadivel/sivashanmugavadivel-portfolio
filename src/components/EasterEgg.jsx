56670fa5ebcbd3ab6d3932bc50d2f5874f0bd887
SIVA SHANMUGA VADIVEL <siva.shanmuga.vadivel-EXT@conti-na.com>
2026-04-05 15:59:25 -0500

    Auto-dismiss easter egg toast after 4.5s

--- src/components/EasterEgg.jsx
+++ src/components/EasterEgg.jsx
@@ -14,6 +14,7 @@ export default function EasterEgg() {
 
   const trigger = useCallback((type) => {
     setToast({ msg: MESSAGES[type], id: Date.now() })
+    setTimeout(() => setToast(null), 4500)
   }, [])
 
   /* Name click via custom event from Home.jsx */
